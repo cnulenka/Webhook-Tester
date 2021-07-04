9784429ec128b146d74b3f8712ca47109be58ab6
cnulenka <sinulenka001@gmail.com>
2021-07-04 19:50:12 +0530

    changes url names

--- src/core/urls.py
+++ src/core/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("create_webhook/", create_webhook),
-    path("post_data/webhook/<str:webhook_endpoint_name>", post_webhook_data),
-    path("get_details/<str:webhook_endpoint_name>", webhook_detail_view),
-    path("get_list/", webhook_list_view),
+    path("create-webhook/", create_webhook),
+    path("post-data/webhook/<str:webhook_endpoint_name>", post_webhook_data),
+    path("get-details/<str:webhook_endpoint_name>", webhook_detail_view),
+    path("get-list/", webhook_list_view),
 ]
